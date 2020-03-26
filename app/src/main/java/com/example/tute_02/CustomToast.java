0f7b220caed85f09a740e536363c5aac67df6868
SammyS1998 <sammysamarasinghe98@gmail.com>
2020-03-26 08:48:42 -0700

    Complete tutorial 02

---
+++ app/src/main/java/com/example/tute_02/CustomToast.java
@@ -0,0 +1,14 @@
+package com.example.tute_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomToast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_toast);
+    }
+}
